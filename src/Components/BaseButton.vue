1c2e9334d0636be85551ad1d926371bf7c339516
Dom Walker <dom.walker1987@gmail.com>
2020-12-16 15:20:13 +0000

    Added config for component library

---
+++ src/Components/BaseButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="base-button">
+    {{ label }}
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator"
+
+@Component
+export default class BaseButton extends Vue {
+  @Prop() private label: string = 'Click me'
+}
+</script>
+
+<style lang="scss" scoped>
+.base-button {
+  background-color: transparent;
+  border: 1px solid blue;
+  color: blue;
+}
+</style>
---
+++ src/Components/BaseInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <input class="base-input" />
+</template>
+
+<script>
+export default {
+  name: 'base-input'
+}
+</script>
+
+<style lang="scss" scoped>
+.base-input {
+  border: 1px solid blue;
+}
+</style>

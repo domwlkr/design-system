f22e422bedda9c164a056310d0954299277c319c
Dom Walker <dom.walker1987@gmail.com>
2020-12-17 10:36:18 +0000

    Fixed issue in index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as components from './components/index'
 
 const install = (instance) => {
   for (let componentKey in components) {
-    instance.user((components)[componentKey])
+    instance.use((components)[componentKey])
   }
 }
 
